76508e2c080876430151c72f73b8b1dd6fcbbc01
진재 김 <rlawlswo6638@gmail.com>
2022-01-09 11:00:24 +0900

    update

--- Client/MultiPlayForm.cs
+++ Client/MultiPlayForm.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace Client
 {
-    public partial class SinglePlayForm : Form
+    public partial class MultiPlayForm : Form
     {
         // 오목판의 크기 15 X 15
         private const int rectSize = 33; // 오목판의 셀 크기
@@ -22,40 +22,35 @@ namespace Client
 
         private bool playing = false;
 
-        public SinglePlayForm()
-        {
-            InitializeComponent();
-        }
-
         private bool judge() // 승리 판정 함수
         {
-            for(int i = 0; i < edgeCount - 4; i++) // 가로
+            for (int i = 0; i < edgeCount - 4; i++) // 가로
             {
-                for(int j = 0; j < edgeCount; j++)
+                for (int j = 0; j < edgeCount; j++)
                 {
                     if (board[i, j] == nowPlayer && board[i + 1, j] == nowPlayer && board[i + 2, j] == nowPlayer && board[i + 3, j] == nowPlayer && board[i + 4, j] == nowPlayer)
                         return true;
                 }
             }
-            for(int i = 0; i < edgeCount; i++) // 세로
+            for (int i = 0; i < edgeCount; i++) // 세로
             {
-                for(int j = 4; j < edgeCount; j++)
+                for (int j = 4; j < edgeCount; j++)
                 {
                     if (board[i, j] == nowPlayer && board[i, j - 1] == nowPlayer && board[i, j - 2] == nowPlayer && board[i, j - 3] == nowPlayer && board[i, j - 4] == nowPlayer)
                         return true;
                 }
             }
-            for(int i = 0; i < edgeCount - 4; i++) // Y = X, 대각선
+            for (int i = 0; i < edgeCount - 4; i++) // Y = X, 대각선
             {
-                for(int j = 0; j < edgeCount - 4; j++)
+                for (int j = 0; j < edgeCount - 4; j++)
                 {
-                    if(board[i,j] == nowPlayer && board[i + 1, j + 1] == nowPlayer && board[i + 2, j + 2] == nowPlayer && board[i + 3, j + 3] == nowPlayer && board[i + 4, j + 4] == nowPlayer)
+                    if (board[i, j] == nowPlayer && board[i + 1, j + 1] == nowPlayer && board[i + 2, j + 2] == nowPlayer && board[i + 3, j + 3] == nowPlayer && board[i + 4, j + 4] == nowPlayer)
                         return true;
                 }
             }
-            for(int i = 4; i > edgeCount; i--) // Y = -X, 대각선
+            for (int i = 4; i > edgeCount; i--) // Y = -X, 대각선
             {
-                for(int j = 0; j < edgeCount - 4; i++)
+                for (int j = 0; j < edgeCount - 4; i++)
                 {
                     if (board[i, j] == nowPlayer && board[i - 1, j + 1] == nowPlayer && board[i - 2, j + 2] == nowPlayer && board[i - 3, j + 3] == nowPlayer && board[i - 4, j + 4] == nowPlayer)
                         return true;
@@ -64,28 +59,18 @@ namespace Client
 
             return false;
         }
-        
-        private void refresh()
-        {
-            this.boardPictureBox.Refresh();
-            for(int i = 0; i < edgeCount; i++)
-            {
-                for(int j = 0; j < edgeCount; j++)
-                    board[i, j] = Horse.none;
-            }
-        }
 
-        private void boardPictureBox_MouseDown(object sender, MouseEventArgs e)
+        private void boardPicture_MouseDown(object sender, MouseEventArgs e)
         {
-            if(!playing)
+            if (!playing)
             {
                 MessageBox.Show("게임을 실행해주세요.");
                 return;
             }
-            Graphics g = this.boardPictureBox.CreateGraphics();
+            Graphics g = this.boardPicture.CreateGraphics();
             int x = e.X / rectSize;
             int y = e.Y / rectSize;
-            if(x < 0 || y < 0 || x >= edgeCount || y >= edgeCount)
+            if (x < 0 || y < 0 || x >= edgeCount || y >= edgeCount)
             {
                 MessageBox.Show("테두리를 벗어날 수 없습니다.");
                 return;
@@ -93,7 +78,7 @@ namespace Client
             if (board[x, y] != Horse.none) return;
             board[x, y] = nowPlayer;
             //MessageBox.Show(x + ", " + y);
-            if(nowPlayer == Horse.BLACK)
+            if (nowPlayer == Horse.BLACK)
             {
                 SolidBrush brush = new SolidBrush(Color.Black);
                 g.FillEllipse(brush, x * rectSize, y * rectSize, rectSize, rectSize);
@@ -103,7 +88,7 @@ namespace Client
                 SolidBrush brush = new SolidBrush(Color.White);
                 g.FillEllipse(brush, x * rectSize, y * rectSize, rectSize, rectSize);
             }
-            if(judge())
+            if (judge())
             {
                 status.Text = nowPlayer.ToString() + "플레이어가 승리했습니다.";
                 playing = false;
@@ -116,7 +101,23 @@ namespace Client
             }
         }
 
-        private void boardPictureBox_Paint(object sender, PaintEventArgs e)
+        private void refresh()
+        {
+            this.boardPicture.Refresh();
+            for (int i = 0; i < edgeCount; i++)
+            {
+                for (int j = 0; j < edgeCount; j++)
+                    board[i, j] = Horse.none;
+            }
+        }
+
+        public MultiPlayForm()
+        {
+            InitializeComponent();
+            this.playButton.Enabled = false;
+        }
+
+        private void boardPicture_Paint(object sender, PaintEventArgs e)
         {
             Graphics gp = e.Graphics;
             Color lineColor = Color.Black; // 오목판의 선 색깔
@@ -132,6 +133,13 @@ namespace Client
                 gp.DrawLine(p, rectSize / 2, i, rectSize * edgeCount - rectSize / 2, i);
                 gp.DrawLine(p, i, rectSize / 2, i, rectSize * edgeCount - rectSize / 2);
             }
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            this.enterButton.Enabled = false;
+            this.playButton.Enabled = true;
+            this.status.Text = "[" + this.roomTextBox.Text + "]번 방에 접속했습니다.";
         }
 
         private void playButton_Click(object sender, EventArgs e)
